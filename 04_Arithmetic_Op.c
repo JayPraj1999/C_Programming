7e1ad5c24dd750421674ab65e9dc8d52c4c6cd30
Mr.Jay <JayPraj1999@users.noreply.github.com>
2022-09-25 12:52:41 +0530

    Arithmetic

---
+++ 04_Arithmetic_Op.c
@@ -0,0 +1,17 @@
+// Arihmetic Operators
+
+#include<stdio.h>
+
+int main()
+{
+	
+ 	int a,b;
+	
+	printf("Enter any two integer value : ");
+	scanf("%i\n%i" , &a,&b);
+	printf("\nAddition is = %i",a+b);
+	printf("\nSubtraction is = %i",a-b);
+	printf("\nMultiplication is = %i",a*b);
+	printf("\nDivision is = %i",a/b);	
+	return 0;
+}
