8c8ff19cc61f135fdf949ea8fbe2c6e049555807
Mr.Jay <JayPraj1999@users.noreply.github.com>
2022-09-27 16:45:55 +0530

    Print String

---
+++ String.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	
+//	char a[20]="Jayprakash";
+
+	char a[20];
+	printf("Please enter your name : ");
+	scanf("%s",&a);
+	printf("\n You Entered : %s",a);
+	printf("%s",a);	
+	return 0;
+	
+}
