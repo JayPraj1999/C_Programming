4615525b9a37db3fdfa48359f3c3fba27633b407
Mr.Jay <JayPraj1999@users.noreply.github.com>
2022-09-29 19:44:00 +0530

    sizeof function

---
+++ size_of_datatype.c
@@ -0,0 +1,25 @@
+//Print size of the datatype
+
+#include<stdio.h>
+
+int main()
+{
+	
+	int a;
+	float b;
+	char c;
+	double d;
+	
+	
+	printf("Size of integer Datatype is %i",sizeof(a));
+	printf("\n\nSize of float Datatype is %i",sizeof(b));
+	printf("\n\nSize of character Datatype is %i",sizeof(c));
+	printf("\n\nSize of double Datatype is %i",sizeof(d));
+	
+	printf("\n\nSize of integer Datatype is %i",sizeof(int));
+	printf("\n\nSize of float Datatype is %i",sizeof(float));
+	printf("\n\nSize of character Datatype is %i",sizeof(char));
+	printf("\n\nSize of double Datatype is %i",sizeof(double));
+	
+	return 0;
+}
