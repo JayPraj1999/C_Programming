3ec23f1aab1ac37dab60d560ba684c1287b29b3d
Mr.Jay <JayPraj1999@users.noreply.github.com>
2022-10-11 19:41:58 +0530

    prime

--- primr_number.c
+++ primr_number.c
@@ -8,21 +8,27 @@ int main()
 	int a,b;
 	printf("Enter Number To check : ");
 	scanf("%i",&a);
-	for(b=2;b<=a;b++)
+	if(a==1)
 	{
-		if(a%b==0)
-		{
-			printf("Not Prine number");
-			break;
-		}
-		else
-		{
-			printf("primr number");
-			break;
-		}
+		printf("\n1 is a prime number.");
 	}
+	else
+	{
+		for(b=2;b<=a;b++)
+		{
+			if(a%b==0)
+			{
+				printf("Not Prine number");
+				break;
+			}
+			else
+			{
+				printf("primr number");
+				break;
+			}
+		}	
 	
-	
+	}
 	
 	return 0;
 }
