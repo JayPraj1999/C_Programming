e83762a33c0b3ff72b812f0a518005cd6a7e1518
Mr.Jay <JayPraj1999@users.noreply.github.com>
2022-10-13 19:13:21 +0530

    Pattern

---
+++ Triangle_number.c
@@ -0,0 +1,12 @@
+//print triangle using numbers.
+
+#include<stdio.h>
+int main()
+{
+	
+	int row,col,x=0;
+	for(row=1;row=5;row++)
+	
+	
+	return 0;
+}
--- pyramid_pattern.c
+++ pyramid_pattern.c
@@ -1,1 +1,22 @@
 //Print pyramid pattern.
+
+#include<stdio.h>
+int main()
+{
+	
+	int row,col;
+	for(row=5;row>=1;row--)
+	{
+		for(col=1;col<=row;col++)
+		{
+			printf(" ");
+		}
+		for(col=5;col>=row;col--)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
